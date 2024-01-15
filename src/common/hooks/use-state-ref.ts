4a80f8648dd04f4f6147a8bd8397a19cfe7e29b7
brandontrabon <brandon.trabon@gmail.com>
2024-01-15 22:23:32 +1000

    BSC-37 - Fixed an issue with the `useStateRef` hook where passing a default value still indicates that the state value can be undefined.

--- src/common/hooks/use-state-ref.ts
+++ src/common/hooks/use-state-ref.ts
@@ -1,12 +1,17 @@
-import { useRef, useState } from 'react';
+import { Dispatch, MutableRefObject, SetStateAction, useRef, useState } from 'react';
 
-const useStateRef = <T>(initialState?: T) => {
+type UseStateRef = {
+  <T>(initialState: T): [T, Dispatch<SetStateAction<T>>, MutableRefObject<T>];
+  <T = undefined>(): [T | undefined, Dispatch<SetStateAction<T | undefined>>, MutableRefObject<T | undefined>];
+};
+
+const useStateRef: UseStateRef = <T>(initialState?: T) => {
   const [state, setState] = useState(initialState);
   const ref = useRef(initialState);
 
   if (ref.current !== state) ref.current = state;
 
-  return [state, setState, ref] as const;
+  return [state, setState, ref];
 };
 
 export { useStateRef };
